da662f7c734bae3d50c7f3539ac0732b49661000
elvismdev <elvismdev@gmail.com>
2016-02-04 06:38:56 -0500

    Adding AWS Service Provider for Symfony

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             new Parenthesis\QBWCAppBundle\ParenthesisQBWCAppBundle(),
+            new \Aws\Symfony\AwsBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
